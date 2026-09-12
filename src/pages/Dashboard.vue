6f47db889824bdae6e9f37356451b517b8573348
I Lava You <laisrmlc@gmail.com>
2026-09-12 21:23:30 +0200

    feat: dashboard functional with filter per genre

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
+import { computed, onBeforeMount, ref } from 'vue'
 
 const shows = ref([])
-const categories = ref([])
+const genres = computed(() => {
+  const genresFromShows = shows.value.flatMap((show) => show.genres)
+
+  return [...new Set(genresFromShows)].sort()
+})
+const selectedGenre = ref('all')
+const filteredShows = computed(() => {
+  if (selectedGenre.value === 'all') {
+    return shows.value
+  }
+
+  return shows.value.filter((show) => show.genres.includes(selectedGenre.value))
+})
 
 const getShows = async () => {
   const response = await fetch('https://api.tvmaze.com/shows')
   return await response.json()
+}
+
+const handleSelectGenre = (genre) => {
+  console.log(genre)
 }
 
 const getGenreList = (shows) => {
@@ -23,23 +39,22 @@ const showGenres = (show) => {
 
 onBeforeMount(async () => {
   shows.value = await getShows()
-  categories.value = getGenreList(shows.value)
 })
 </script>
 
 <template>
   <h1>TV SHOWS</h1>
-  <!--CATEGORIES-->
-  <select>
-    <option value="all">All Genres</option>
-    <option v-for="category in categories" :key="category" :value="category">
+  <!--genres-->
+  <select v-model="selectedGenre">
+    <option value="all" key="all">All genres</option>
+    <option v-for="category in genres" :key="category" :value="category" @click="handleSelectGenre">
       {{ category }}
     </option>
   </select>
 
   <!--TV SHOWS LIST-->
   <div>
-    <div v-for="show in shows" :key="show.id">
+    <div v-for="show in filteredShows" :key="show.id">
       <img :src="show.image.medium" :alt="show.name" />
       <h2>{{ show.name }}</h2>
       <p>{{ showGenres(show) }}</p>
