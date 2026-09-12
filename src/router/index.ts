bd5ee864c4c10964d060938cb69c34951cb6313a
Laís Carvalho <laisrmlc@gmail.com>
2026-09-12 19:09:38 +0200

    feat: adding the 2 pages from the project and setting the routers (#2)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../pages/Dashboard.vue'
+import ShowDetails from '../pages/ShowDetails.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: 'dashboard', component: Dashboard },
+    { path: '/show-details/:id', name: 'show-details', component: ShowDetails },
+  ],
 })
 
 export default router
